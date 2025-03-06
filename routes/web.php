abd4c233490a6700a273edc482fe91c1fab37b69
vinoles <felipe.vinoles@gmail.com>
2025-03-06 21:28:57 +0100

    redirect to api form home

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect(route('l5-swagger.default.api'));
 });
 
 Route::get('/redoc-api', function () {
