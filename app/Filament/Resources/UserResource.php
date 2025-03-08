bac8878e7618302eb5465650cb23c6e7a4bc553e
vinoles <felipe.vinoles@gmail.com>
2025-03-08 17:47:31 +0100

    remove locale unused

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -24,7 +24,7 @@ class UserResource extends Resource
 {
     protected static ?string $model = User::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-s-users';
 
     public static function getNavigationGroup(): string
     {
