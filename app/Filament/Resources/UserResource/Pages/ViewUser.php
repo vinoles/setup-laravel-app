db6b073788801019193dc445f505415035642710
vinoles <felipe.vinoles@gmail.com>
2025-03-08 17:00:55 +0100

    add view user page to admin panel

---
+++ app/Filament/Resources/UserResource/Pages/ViewUser.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Filament\Resources\UserResource\Pages;
+
+use App\Filament\Resources\UserResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ViewRecord;
+
+class ViewUser extends ViewRecord
+{
+    protected static string $resource = UserResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+            Actions\ForceDeleteAction::make(),
+            Actions\RestoreAction::make(),
+        ];
+    }
+}
