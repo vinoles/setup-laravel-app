e388d2d654c93ba0875139998cbba76448a10752
vinoles <felipe.vinoles@gmail.com>
2025-03-08 16:27:38 +0100

    add custom translations

---
+++ lang/fr/admin.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+    'locales' => [
+        'es' => 'Spanish',
+        'en' => 'English',
+        'fr' => 'French',
+    ],
+    'globals' => [
+        'first_name' => 'First name',
+        'second_name' => 'Second name',
+        'full_name' => 'Full name',
+        'last_name' => 'Last name',
+        'first_last_name' => 'First last name',
+        'second_last_name' => 'Second last name',
+        'social' => 'Social',
+        'phone' => 'Phone',
+        'address' => 'Address',
+        'city' => 'City',
+        'birthdate' => 'Birthdate',
+        'postal_code' => 'Postal code',
+        'province' => 'Province',
+        'created_at' => 'Created At',
+        'updated_at' => 'Updated At',
+        'deleted_at' => 'Deleted At'
+    ],
+    'users' => [
+        'users' => 'Users',
+        'user' => 'User',
+    ]
+];
