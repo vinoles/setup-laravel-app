aa140d307ed01af0f81a131849252ce53f9377c3
vinoles <felipe.vinoles@gmail.com>
2025-03-08 17:04:06 +0100

    remove namespaces in view page

--- app/Filament/Resources/UserResource/Pages/ViewUser.php
+++ app/Filament/Resources/UserResource/Pages/ViewUser.php
@@ -3,7 +3,9 @@
 namespace App\Filament\Resources\UserResource\Pages;
 
 use App\Filament\Resources\UserResource;
-use Filament\Actions;
+use Filament\Actions\DeleteAction;
+use Filament\Actions\ForceDeleteAction;
+use Filament\Actions\RestoreAction;
 use Filament\Resources\Pages\ViewRecord;
 
 class ViewUser extends ViewRecord
@@ -13,9 +15,9 @@ class ViewUser extends ViewRecord
     protected function getHeaderActions(): array
     {
         return [
-            Actions\DeleteAction::make(),
-            Actions\ForceDeleteAction::make(),
-            Actions\RestoreAction::make(),
+            DeleteAction::make(),
+            ForceDeleteAction::make(),
+            RestoreAction::make(),
         ];
     }
 }
