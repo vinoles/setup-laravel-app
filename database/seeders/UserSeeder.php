b10eb72061bcd3d03d049b1ae2dbeff71d27bb9e
vinoles <felipe.vinoles@gmail.com>
2025-09-06 14:21:02 +0200

    adapter users and roles seeders

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Constants\UserRole;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -13,21 +14,60 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory(10)->create();
-
         $password = env('TEST_PASSWORD', 'password');
 
-        User::factory()->create([
+        // Create random users with different roles
+        User::factory(5)->create()->each(function ($user) {
+            $roles = [UserRole::TALENT->value, UserRole::SCOUT->value, UserRole::CLUB->value];
+            $user->assignRole($roles[array_rand($roles)]);
+        });
+
+        // Create specific test users with known roles
+        $admin = User::factory()->create([
             'first_name' => 'Admin',
             'last_name' => 'Application',
             'email' => 'admin@app.com',
             'password' => Hash::make($password),
         ]);
+        $admin->assignRole(UserRole::ADMIN->value);
 
+        $talent = User::factory()->create([
+            'first_name' => 'Lionel',
+            'last_name' => 'Messi',
+            'email' => 'messi@app.com',
+            'password' => Hash::make($password),
+        ]);
+        $talent->assignRole(UserRole::TALENT->value);
+
+        $scout = User::factory()->create([
+            'first_name' => 'Jose',
+            'last_name' => 'Mourinho',
+            'email' => 'mourinho@app.com',
+            'password' => Hash::make($password),
+        ]);
+        $scout->assignRole(UserRole::SCOUT->value);
+
+        $club = User::factory()->create([
+            'first_name' => 'Real',
+            'last_name' => 'Madrid',
+            'email' => 'realmadrid@app.com',
+            'password' => Hash::make($password),
+        ]);
+        $club->assignRole(UserRole::CLUB->value);
+
+        $superAdmin = User::factory()->create([
+            'first_name' => 'Super',
+            'last_name' => 'Admin',
+            'email' => 'superadmin@app.com',
+            'password' => Hash::make($password),
+        ]);
+        $superAdmin->assignRole(UserRole::SUPER_ADMIN->value);
+
+        // Create a user without role for testing
         User::factory()->create([
-            'first_name' => 'User',
-            'last_name' => 'Test',
-            'email' => 'user-test@app.com',
+            'first_name' => 'No',
+            'last_name' => 'Role',
+            'email' => 'norole@app.com',
             'password' => Hash::make($password),
         ]);
     }
