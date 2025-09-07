6e1732ffd5799c98a846b27d33db042b9f4871c0
vinoles <felipe.vinoles@gmail.com>
2025-09-07 19:57:39 +0200

    update max limit address attribute

--- app/JsonApi/V1/Users/UserRequest.php
+++ app/JsonApi/V1/Users/UserRequest.php
@@ -36,11 +36,11 @@ class UserRequest extends ResourceRequest
                 'date',
                 "before_or_equal:{$allowedBirthdate}",
             ],
-            'address' => ['nullable', 'max:50'],
+            'address' => ['nullable', 'max:150'],
             'city' => ['nullable', 'max:50'],
             'country' => ['nullable', 'max:50'],
             'postal_code' => ['nullable', 'max:50'],
-            'phone' => ['nullable', 'max:50'],
+            'phone' => ['nullable', 'max:20'],
             'password' => $this->passwordRules(),
         ];
     }
