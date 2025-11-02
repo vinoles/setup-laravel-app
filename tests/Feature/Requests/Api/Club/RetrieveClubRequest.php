5006744f1ff69b1406ad96a70333777aba1b9313
vinoles <felipe.vinoles@gmail.com>
2025-11-02 15:32:58 +0100

    implement retrieve club api test

---
+++ tests/Feature/Requests/Api/Club/RetrieveClubRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature\Requests\Api\Club;
+
+use App\Models\Club;
+use Tests\Feature\Requests\GetRequest;
+
+class RetrieveClubRequest extends GetRequest
+{
+    /**
+     * Create a new instance of the request.
+     *
+     * @param  Club|null  $club
+     */
+    public function __construct(protected Club|null $club = null)
+    {
+        if ($club === null) {
+            $this->club = $club = Club::factory()->create();
+        }
+    }
+
+    /**
+     * Retrieve the endpoint of the request.
+     *
+     * @return string
+     */
+    public function endpoint(): string
+    {
+        return route('v1.api.clubs.show', ['club' => $this->club]);
+    }
+}
+
