f27ccbd002cb09b8c51816cacfe1eb9fdab53023
Ildemaro Rodríguez <ildemaro88@gmail.com>
2025-11-04 22:58:23 +0100

    fix: Remove incorrectly attribute uuid
    According to the JSON:API specification, a resource object's id is a top-level member, not part of the attributes object. This schema for the attributes object incorrectly includes uuid. To align with the specification and avoid confusion, this property should be removed from the attributes documentation

--- app/ApiDoc/Player/Schemas/PlayerAttributes.php
+++ app/ApiDoc/Player/Schemas/PlayerAttributes.php
@@ -8,11 +8,6 @@ use OpenApi\Annotations as OA;
  * @OA\Schema(
  *     type="object",
  *     @OA\Property(
- *         property="uuid",
- *         type="string",
- *         example="0ec37904-ce76-4e3a-b6c5-0a3b77d70e54"
- *     ),
- *     @OA\Property(
  *         property="first_name",
  *         type="string",
  *         example="Lionel"
