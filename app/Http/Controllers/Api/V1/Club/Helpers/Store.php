965d133ecfe404fe02954d5abb982b34bc5154a9
vinoles <felipe.vinoles@gmail.com>
2025-11-11 20:46:08 +0100

    feat: Remove import unused

--- app/Http/Controllers/Api/V1/Club/Helpers/Store.php
+++ app/Http/Controllers/Api/V1/Club/Helpers/Store.php
@@ -5,10 +5,8 @@ namespace App\Http\Controllers\Api\V1\Club\Helpers;
 use App\Helpers\ApiResponseHelper;
 use App\Jobs\Clubs\CreateClub;
 use App\JsonApi\V1\Clubs\ClubRequest;
-use App\JsonApi\V1\Clubs\ClubSchema;
 use Illuminate\Http\Response;
 use Illuminate\Support\Str;
-use LaravelJsonApi\Laravel\Http\Requests\AnonymousQuery;
 
 trait Store
 {
